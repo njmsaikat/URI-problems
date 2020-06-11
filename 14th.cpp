bae36f7a74b0d46c8a68e96ec3fb2d125e777b40
Saikat Roy <njmsaikat@gmail.com>
2020-06-11 13:11:58 +0600

    14th solved

---
+++ 14th.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+int x;
+double y;
+
+cin>>x>>fixed>>setprecision(1)>>y;
+
+cout<<fixed<<setprecision(3)<<x/y<<" km/l"<<endl
+return 0;
+}
