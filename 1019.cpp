5056998773c76273a5889d04c05c3077d4bf2ae2
Saikat Roy <njmsaikat@gmail.com>
2020-10-18 22:12:27 +0600

    19th solved

---
+++ 1019.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n,h,m,s;
+    cin>>n;
+    h=n/3600;
+    m=(n%3600)/60;
+    s= n%60;
+    cout<<h<<":"<<m<<":"<<s<<endl;
+    return 0;
+}
